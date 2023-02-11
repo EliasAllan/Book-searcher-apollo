74a90de095af92788922262bb09fd026b552b9d9
AllanEl <allanrnelias@gmail.com>
2023-02-10 23:24:16 -0500

    refactored code to use Apollo as a server

---
+++ server/schemas/index.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+//this needs to be looked at, its a copy/paste from mini project
+
+
+
+
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
